bef6f94179c1008832ee0ae66d9e56df4f566cfe
fuling <mon.samuel@qq.com>
2022-02-04 23:10:08 +0800

    ✨ add updateFungibleTarget and fix balance

--- contracts/extensions/ComposableWithERC20.sol
+++ contracts/extensions/ComposableWithERC20.sol
@@ -8,6 +8,7 @@ abstract contract ComposableWithERC20 is Composable {
     using SafeERC20 for IERC20;
 
     mapping(address => mapping(uint256 => uint256)) _balances;
+
     // can also be linkERC20
     function linkFungible(
         address fungibleTokenAddress,
@@ -26,7 +27,45 @@ abstract contract ComposableWithERC20 is Composable {
             value
         );
 
-        _balances[targetTokenAddress][targetTokenId] = value;
+        uint256 oldBalance = _balances[targetTokenAddress][targetTokenId];
+        _balances[targetTokenAddress][targetTokenId] = oldBalance + value;
+    }
+
+    function updateFungibleTarget(
+        address sourceTokenAddress,
+        uint256 sourceTokenId,
+        address fungibleTokenAddress,
+        uint256 value,
+        address targetTokenAddress,
+        uint256 targetTokenId
+    ) public {
+        require(
+            _checkItemsExists(targetTokenAddress, targetTokenId),
+            "target/parent token token not in contract"
+        );
+        require(
+            _checkItemsExists(sourceTokenAddress, sourceTokenId),
+            "source/child token token not in contract"
+        );
+
+        (address rootTokenAddress, uint256 rootTokenId) = findRootToken(
+            sourceTokenAddress,
+            sourceTokenId
+        );
+        require(
+            ERC721(sourceTokenAddress).ownerOf(rootTokenId) == msg.sender,
+            "caller not owner of source token"
+        );
+        require(
+            balanceOfFungible(sourceTokenAddress, sourceTokenId) >= value,
+            "transfer amount exceeds balance"
+        );
+
+        uint256 oldSourceBalance = _balances[sourceTokenAddress][sourceTokenId];
+        _balances[sourceTokenAddress][sourceTokenId] = oldSourceBalance - value;
+
+        uint256 oldTargetBalance = _balances[targetTokenAddress][targetTokenId];
+        _balances[targetTokenAddress][targetTokenId] = oldTargetBalance + value;
     }
 
     function unlinkFungible(
@@ -52,6 +91,9 @@ abstract contract ComposableWithERC20 is Composable {
             balanceOfFungible(targetTokenAddress, targetTokenId) >= value,
             "transfer amount exceeds balance"
         );
+
+        uint256 oldBalance = _balances[targetTokenAddress][targetTokenId];
+        _balances[targetTokenAddress][targetTokenId] = oldBalance - value;
 
         IERC20(fungibleTokenAddress).safeTransfer(to, value);
     }
