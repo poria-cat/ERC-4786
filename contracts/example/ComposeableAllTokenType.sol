31328f8bb3d56b42fcda6802ee5e5ae9e2e8c941
fuling <mon.samuel@qq.com>
2022-02-06 21:13:22 +0800

    ✨ add ComposeableAllTokenType example

---
+++ contracts/example/ComposeableAllTokenType.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL3.0
+
+pragma solidity ^0.8.0;
+
+import "../Composable.sol";
+import "../extensions/ComposableWithERC20.sol";
+import "../extensions/ComposableWithERC1155.sol";
+
+contract ComposeableAllTokenType is
+    Composable,
+    ComposableWithERC20,
+    ComposableWithERC1155
+{
+    constructor(string memory _tokenName, string memory _tokenSymbol)
+        Composable(_tokenName, _tokenSymbol)
+    {}
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(Composable, ComposableWithERC20, ComposableWithERC1155)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
