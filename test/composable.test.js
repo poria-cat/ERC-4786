0f2816f475afcd8927254e6e421312653871a61f
fuling <mon.samuel@qq.com>
2022-02-08 13:32:27 +0800

    ✅ update composable test

--- test/composable.test.js
+++ test/composable.test.js
@@ -8,7 +8,8 @@ describe("Test Composable", function () {
   let accounts;
 
   let composable;
-  let testNFT;
+  let targetNFT;
+  let mockNFT;
 
   const testTokenId0 = 0;
   const testTokenId1 = 1;
@@ -18,240 +19,285 @@ describe("Test Composable", function () {
 
   before(async function () {
     this.Composable = await ethers.getContractFactory("Composable");
-    this.TestNFT = await ethers.getContractFactory("TestNFT");
+    this.TargetNFT = await ethers.getContractFactory("MockNFT");
+    this.MockNFT = await ethers.getContractFactory("MockNFT");
 
     accounts = await ethers.getSigners();
   });
 
   beforeEach(async function () {
-    composable = await this.Composable.deploy("JustNFT", "JNFT");
+    composable = await this.Composable.deploy();
     await composable.deployed();
 
-    testNFT = await this.TestNFT.deploy();
-    await testNFT.deployed();
+    targetNFT = await this.TargetNFT.deploy();
+    await targetNFT.deployed();
+
+    mockNFT = await this.MockNFT.deploy();
+    await mockNFT.deployed();
   });
 
   describe("Test link", function () {
-    it("link NFT in another contract", async function () {
-      await composable.safeMint(accounts[0].address);
-      await composable.safeMint(accounts[0].address);
+    it("link", async () => {
+      await targetNFT.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
 
-      await testNFT.setApprovalForAll(composable.address, true);
-      // link test NFT 0  to target nft 1
-      await composable.link(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId1]
-      );
+      await mockNFT.setApprovalForAll(composable.address, true);
 
-      const sourceToken = [testNFT.address, testTokenId1];
-      const targetToken = [testNFT.address, testTokenId0];
-      // link test nft 1 to test nft 0
+      let sourceToken = [mockNFT.address, testTokenId0];
+      let targetToken = [targetNFT.address, linkedTokenId1];
+
       await composable.link(sourceToken, targetToken);
 
-      expect(
-        (await composable.findRootToken([testNFT.address, testTokenId0]))[1]
-      ).to.be.eq(BigNumber.from(linkedTokenId1));
+      sourceToken = [mockNFT.address, testTokenId1];
+      targetToken = [mockNFT.address, testTokenId0];
+
+      await composable.link(sourceToken, targetToken);
+
+      const rootToken = await composable.findRootToken([
+        mockNFT.address,
+        testTokenId1,
+      ]);
+
+      expect(rootToken[0]).to.be.eq(targetNFT.address);
+      expect(rootToken[1]).to.be.eq(BigNumber.from(linkedTokenId1));
     });
 
-    it("can't link a NFT that is not owned", async function () {
-      await testNFT.connect(accounts[1]).safeMint(accounts[1].address);
-      await testNFT.setApprovalForAll(composable.address, true);
+    it("check ancestor", async () => {
+      await targetNFT.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await composable.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+
+      await mockNFT.setApprovalForAll(composable.address, true);
+
+      const sourceToken = [mockNFT.address, testTokenId1];
+      const targetToken = [mockNFT.address, testTokenId0];
+
+      await composable.link(sourceToken, targetToken);
+      await expectRevert(
+        composable.link(targetToken, sourceToken),
+        "source token is ancestor token"
+      );
+    });
+
+    it("can't link a NFT that is not owned", async () => {
+      await mockNFT.connect(accounts[1]).safeMint(accounts[1].address);
+      await mockNFT.setApprovalForAll(composable.address, true);
+
+      await targetNFT.safeMint(accounts[0].address);
 
       await expectRevert(
         composable.link(
-          [testNFT.address, testTokenId0],
-          [composable.address, linkedTokenId0]
+          [mockNFT.address, testTokenId0],
+          [targetNFT.address, linkedTokenId0]
         ),
         "ERC721: transfer caller is not owner nor approved"
       );
     });
 
-    it("can't link a NFT that is not owned (approved)", async function () {
-      await testNFT.connect(accounts[1]).safeMint(accounts[1].address);
-      await testNFT.setApprovalForAll(composable.address, true);
-      await testNFT
+    it("can't link a NFT that is not owned (approved)", async () => {
+      await mockNFT.connect(accounts[1]).safeMint(accounts[1].address);
+      await mockNFT.setApprovalForAll(composable.address, true);
+      await mockNFT
         .connect(accounts[1])
         .setApprovalForAll(composable.address, true);
 
-      await composable.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
       await expectRevert(
         composable.link(
-          [testNFT.address, testTokenId0],
-          [composable.address, linkedTokenId0]
+          [mockNFT.address, testTokenId0],
+          [targetNFT.address, linkedTokenId0]
         ),
         "ERC721: transfer of token that is not own"
       );
     });
 
-    it("can't link a NFT to not exists in composable contract", async function () {
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.safeMint(accounts[1].address);
-      await testNFT.setApprovalForAll(composable.address, true);
+    it("can't link to not erc721 token", async () => {
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.setApprovalForAll(composable.address, true);
 
-      await composable.safeMint(accounts[0].address);
+      const sourceToken = [mockNFT.address, testTokenId0];
+      const targetToken = [composable.address, linkedTokenId0];
+
       await expectRevert(
-        composable.link(
-          [testNFT.address, testTokenId0],
-          [testNFT.address, testTokenId1]
-        ),
-        "target/parent token not in contract"
+        composable.link(sourceToken, targetToken),
+        "target/parent token not ERC721 token or not exist"
+      );
+      await expectRevert(
+        composable.link(targetToken, sourceToken),
+        "source/child token not ERC721 token or not exist"
       );
     });
   });
 
-  describe("Test updateTarget", async function () {
-    it("updateTarget", async function () {
-      await composable.safeMint(accounts[0].address);
-      await composable.safeMint(accounts[0].address);
+  describe("Test updateTarget", async () => {
+    it("updateTarget", async () => {
+      await targetNFT.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
 
-      await testNFT.setApprovalForAll(composable.address, true);
+      await mockNFT.setApprovalForAll(composable.address, true);
       // link test NFT 0  to target nft 1
       await composable.link(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId1]
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId1]
       );
 
       let root = await composable.findRootToken([
-        testNFT.address,
+        mockNFT.address,
         testTokenId0,
       ]);
 
-      expect(root[0]).to.be.eq(composable.address);
+      expect(root[0]).to.be.eq(targetNFT.address);
       expect(root[1]).to.be.eq(BigNumber.from(linkedTokenId1));
 
       await composable.updateTarget(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId0]
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId0]
       );
 
-      root = await composable.findRootToken([testNFT.address, testTokenId0]);
+      root = await composable.findRootToken([mockNFT.address, testTokenId0]);
 
-      expect(root[0]).to.be.eq(composable.address);
+      expect(root[0]).to.be.eq(targetNFT.address);
       expect(root[1]).to.be.eq(BigNumber.from(linkedTokenId0));
     });
 
-    it("can't updateTarget to nft that not exist in contract", async function () {
-      await composable.safeMint(accounts[0].address);
-      await composable.safeMint(accounts[0].address);
+    it("can't updateTarget to descendant token", async () => {
+      await targetNFT.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
 
-      await testNFT.setApprovalForAll(composable.address, true);
+      await mockNFT.setApprovalForAll(composable.address, true);
 
       await composable.link(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId1]
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId1]
       );
-      // await composable.updateTarget([testNFT.address, testTokenId0], [testNFT.address, testTokenId1])
+
       await expectRevert(
         composable.updateTarget(
-          [testNFT.address, testTokenId0],
-          [testNFT.address, testTokenId1]
+          [targetNFT.address, linkedTokenId1],
+          [mockNFT.address, testTokenId0]
         ),
-        "target/parent token not in contract"
+        "source token is ancestor token"
       );
     });
 
-    it("can't updateTarget with not own", async function () {
-      await composable.safeMint(accounts[0].address);
-      await composable.safeMint(accounts[0].address);
+    it("can't updateTarget with not own", async () => {
+      await targetNFT.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
 
-      await testNFT.setApprovalForAll(composable.address, true);
+      await mockNFT.setApprovalForAll(composable.address, true);
 
       await composable.link(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId1]
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId1]
       );
 
       expectRevert(
         composable
           .connect(accounts[1])
           .updateTarget(
-            [testNFT.address, testTokenId0],
-            [composable.address, linkedTokenId0]
+            [mockNFT.address, testTokenId0],
+            [targetNFT.address, linkedTokenId0]
           ),
         "caller is not owner of source/child token"
       );
     });
+
+    it("can't update target to not erc721 token", async () => {
+      await targetNFT.safeMint(accounts[0].address);
+
+      await mockNFT.safeMint(accounts[0].address);
+
+      await mockNFT.setApprovalForAll(composable.address, true);
+
+      await composable.link(
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId0]
+      );
+
+      await expectRevert(
+        composable.updateTarget(
+          [mockNFT.address, testTokenId0],
+          [composable.address, linkedTokenId0]
+        ),
+        "target/parent token not ERC721 token or not exist"
+      );
+    });
   });
 
-  describe("Test unlink", async function () {
-    it("unlink", async function () {
-      await composable.safeMint(accounts[0].address);
-      await composable.safeMint(accounts[0].address);
+  describe("Test unlink", async () => {
+    it("unlink", async () => {
+      await targetNFT.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
 
-      await testNFT.setApprovalForAll(composable.address, true);
+      await mockNFT.setApprovalForAll(composable.address, true);
       // parent NFT 1 => test NFT 0
       await composable.link(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId1]
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId1]
       );
       // parent NFT 1 => test NFT 0 => test NFT 1
       await composable.link(
-        [testNFT.address, testTokenId1],
-        [testNFT.address, testTokenId0]
+        [mockNFT.address, testTokenId1],
+        [mockNFT.address, testTokenId0]
       );
 
       await composable.unlink(accounts[1].address, [
-        testNFT.address,
+        mockNFT.address,
         testTokenId1,
       ]);
     });
 
     it("can't unlink not own", async function () {
-      await composable.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.setApprovalForAll(composable.address, true);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.setApprovalForAll(composable.address, true);
 
       await composable.link(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId0]
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId0]
       );
       await expectRevert(
         composable
           .connect(accounts[1])
-          .unlink(accounts[1].address, [testNFT.address, testTokenId0]),
+          .unlink(accounts[1].address, [mockNFT.address, testTokenId0]),
         "caller is not owner of source/child token"
       );
     });
 
     it("can't unlink not exists in contract's nft", async function () {
-      await composable.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.setApprovalForAll(composable.address, true);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.setApprovalForAll(composable.address, true);
 
       await expectRevert(
-        composable.unlink(accounts[0].address, [testNFT.address, testTokenId0]),
+        composable.unlink(accounts[0].address, [mockNFT.address, testTokenId0]),
         "source/child token not in contract"
       );
     });
 
     it("can't unlink to zero address", async () => {
-      await composable.safeMint(accounts[0].address);
-
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.setApprovalForAll(composable.address, true);
-
       await expectRevert(
         composable.unlink(constants.ZERO_ADDRESS, [
-          testNFT.address,
+          mockNFT.address,
           testTokenId0,
         ]),
         "can't unlink to zero address"
@@ -259,34 +305,34 @@ describe("Test Composable", function () {
     });
 
     it("complex", async () => {
-      await composable.safeMint(accounts[0].address);
-      await composable.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
+      await targetNFT.safeMint(accounts[0].address);
 
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.safeMint(accounts[0].address);
-      await testNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
+      await mockNFT.safeMint(accounts[0].address);
 
-      await testNFT.setApprovalForAll(composable.address, true);
+      await mockNFT.setApprovalForAll(composable.address, true);
 
       // 1. link to contract
       await composable.link(
-        [testNFT.address, testTokenId0],
-        [composable.address, linkedTokenId0]
+        [mockNFT.address, testTokenId0],
+        [targetNFT.address, linkedTokenId0]
       );
       // 2. link to last
       await composable.link(
-        [testNFT.address, testTokenId1],
-        [testNFT.address, testTokenId0]
+        [mockNFT.address, testTokenId1],
+        [mockNFT.address, testTokenId0]
       );
       // 3. unlink
       await composable.unlink(accounts[0].address, [
-        testNFT.address,
+        mockNFT.address,
         testTokenId0,
       ]);
       // 4. link
       await composable.link(
-        [testNFT.address, 2],
-        [testNFT.address, testTokenId0]
+        [mockNFT.address, 2],
+        [mockNFT.address, testTokenId0]
       );
     });
   });
